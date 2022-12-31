5d4c16d57aa2ea1e22833918aa6645aec310b569
magiccode <magic.code@outlook.com>
2022-12-31 14:43:35 +1100

    Change block color

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@
 /**
  * TM1650 digit Display
  */
-//% weight=100 color=#64C800 icon="\uf1b9" block="Magic 4-LED"
+//% weight=100 color=#31C7D5 icon="\uf1b9" block="Magic 4-LED"
 namespace TM1650 {
 
     let COMMAND_I2C_ADDRESS = 0x24
